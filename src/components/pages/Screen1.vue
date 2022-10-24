2cf2289e7714f59ec3a3730bbe16d2ed6d96032a
NUCAL <yagmur.sahin@nucal.com>
2022-10-24 15:27:33 +0300

    NUCAL-UI | Front-end application initial commit, powered by NUCAL development team.

--- src/components/pages/Screen1.vue
+++ src/components/pages/Screen1.vue
@@ -9,14 +9,24 @@
       v-bind:marginTop="0" v-bind:marginLeft="0" v-bind:marginRight="0" v-bind:marginBottom="0"
       v-bind:style="{'height':'auto','background':'transparent','paddingTop':'0px','borderColor':'','borderStyle':'solid','borderWidth':'0px','paddingLeft':'0px','borderRadius':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'1px','borderTopRightRadius':'1px','borderBottomLeftRadius':'1px','borderBottomRightRadius':'1px'}"
       slot="r0-c0" area="r0-c0">
-      <WRoundedBottomImage v-bind:blank="false" rounded="bottom" stretch="none"
+      <WRoundedBottomImage top="33.45583596214511%" left="44.53260869565217%" v-bind:blank="false" rounded="bottom"
+        stretch="none"
         imageUrl="https://img.freepik.com/free-vector/cute-koala-sleeping-cartoon-illustration_138676-2778.jpg?w=2000"
+        position="absolute" v-bind:isEnabled="true" v-bind:thumbnail="false" blankColor="black"
+        v-bind:aspectRatio="true" dataLoading="static" ncTooltipText="Please write a text to display in tooltip..."
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'type':'string','update':{'type':'dataModelForDynamicType'},'propName':'imageUrl','displayName':'Value'}]}"
+        ncClickBorderColor="black" ncHoverBorderColor="black" dataModelForDynamicType="string"
+        id="56aeda56-c648-4f87-91eb-692b83d9f8c7"
+        v-bind:style="{'width':'281.5px','height':'140.75px','zIndex':2,'maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderColor':'#4B74FF','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}"/>
+      <WImage v-bind:blank="false" v-bind:rounded="false" stretch="none"
+        imageUrl="https://images.mubicdn.net/images/film/73520/cache-60565-1445925123/image-w1280.jpg"
         v-bind:isEnabled="true" v-bind:thumbnail="false" blankColor="black" v-bind:aspectRatio="true"
         dataLoading="static" ncTooltipText="Please write a text to display in tooltip..."
         v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'update':{'type':'dataModelForDynamicType'},'propName':'imageUrl','displayName':'Value'}]}"
         ncClickBorderColor="black" ncHoverBorderColor="black" dataModelForDynamicType="string" position="absolute"
-        top="33.45583596214511%" left="44.53260869565217%" id="56aeda56-c648-4f87-91eb-692b83d9f8c7"
-        v-bind:style="{'width':'281.5px','height':'140.75px','maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderColor':'#4B74FF','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','zIndex':2}"/>
+        top="10.883280757097792%" left="13.91304347826087%" v-bind:snapX="false" v-bind:snapY="false"
+        id="69ae91ad-5ecc-47e8-93a5-2d808f4bfc75"
+        v-bind:style="{'width':'281.5px','height':'140.75px','maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderColor':'#4B74FF','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','zIndex':3}"/>
     </NucGridCell>
   </NucGridPage>
 </template>
